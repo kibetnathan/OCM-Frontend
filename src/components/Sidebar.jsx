0defd21f0c6ebdc76e352096a801595f361ae1d7
kibetnathan <kibetnat@gmail.com>
2026-02-26 19:08:52 +0300

    update favicon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -115,7 +115,8 @@ function Sidebar() {
               >
                 <li><NavLink to="/dashboard/groups/fellowship" className={subLinkClass}>Fellowship Groups</NavLink></li>
                 <li><NavLink to="/dashboard/groups/classes" className={subLinkClass}>Classes</NavLink></li>
-                <li><NavLink to="/dashboard/groups/serving-teams" className={subLinkClass}>Serving Teams</NavLink></li>
+                <li><NavLink to="/dashboard/groups/serving-teams" className={subLinkClass}>Serving Departments</NavLink></li>
+                <li><NavLink to="/dashboard/groups/serving-teams" className={subLinkClass}>Church Services</NavLink></li>
               </ExpandableSection>
 
               <ExpandableSection
